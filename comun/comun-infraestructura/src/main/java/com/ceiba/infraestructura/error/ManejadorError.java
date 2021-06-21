9b08d7eb7ef63c5320d67584278a2c5e868732f8
adelgadillog <4lejo.1017@gmail.com>
2021-06-21 15:04:44 -0500

    Commit

--- comun/comun-infraestructura/src/main/java/com/ceiba/infraestructura/error/ManejadorError.java
+++ comun/comun-infraestructura/src/main/java/com/ceiba/infraestructura/error/ManejadorError.java
@@ -3,6 +3,7 @@ package com.ceiba.infraestructura.error;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.ceiba.dominio.excepcion.*;
 import com.ceiba.infraestructura.excepcion.ExcepcionTecnica;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,12 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
-import com.ceiba.dominio.excepcion.ExcepcionLongitudValor;
-import com.ceiba.dominio.excepcion.ExcepcionSinDatos;
-import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
-import com.ceiba.dominio.excepcion.ExcepcionValorObligatorio;
 
 @ControllerAdvice
 public class ManejadorError extends ResponseEntityExceptionHandler {
@@ -33,8 +28,9 @@ public class ManejadorError extends ResponseEntityExceptionHandler {
         CODIGOS_ESTADO.put(ExcepcionSinDatos.class.getSimpleName(), HttpStatus.NOT_FOUND.value());
         CODIGOS_ESTADO.put(ExcepcionValorObligatorio.class.getSimpleName(), HttpStatus.BAD_REQUEST.value());
         CODIGOS_ESTADO.put(ExcepcionDuplicidad.class.getSimpleName(), HttpStatus.BAD_REQUEST.value());
+        CODIGOS_ESTADO.put(ExcepcionCantidadNoDisponible.class.getSimpleName(), HttpStatus.BAD_REQUEST.value());
         CODIGOS_ESTADO.put(ExcepcionTecnica.class.getSimpleName(), HttpStatus.INTERNAL_SERVER_ERROR.value());
-        
+
         
         //en caso de tener otra excepcion matricularla aca
     }
--- microservicio/infraestructura/src/main/java/com/ceiba/trm/controlador/ConsultaControladorTrm.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/trm/controlador/ConsultaControladorTrm.java
@@ -4,6 +4,7 @@ import com.ceiba.trm.consulta.ManejadorObtenerTrm;
 import com.ceiba.trm.modelo.DtoTrm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.rmi.RemoteException;
 @RestController
 @RequestMapping("/trm")
 @Api(tags={"Controlador consulta TRM del dia"})
+@CrossOrigin("*")
 public class ConsultaControladorTrm {
 
     private final ManejadorObtenerTrm manejadorObtenerTrm;
